c3c7d6b3cf0d968256f5c89203ae19a80983c5b9
Younis Alaradi <67017967+younis-alaradi@users.noreply.github.com>
2022-10-25 20:03:12 +0300

    LAST DB - 25/10/2022

--- jassara.sql
+++ jassara.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 22, 2022 at 07:41 PM
+-- Generation Time: Oct 25, 2022 at 07:01 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
